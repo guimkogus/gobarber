7ede5ec2bb78b75592ccd0678294aa09eb32c0cd
guimkogus <guilherme.mkogus@gmail.com>
2020-02-20 15:20:17 -0300

    created user controller

---
+++ backend/src/app/controllers/UserController.js
@@ -0,0 +1,24 @@
+/* eslint-disable class-methods-use-this */
+import User from '../models/Users';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({
+      where: { name: req.body.name, email: req.body.email },
+    });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+}
+
+export default new UserController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/Users';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Guilherme M. KOGUS',
-    email: 'guilherme.mkogus@gmail.com',
-    password_hash: '12345678',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 module.exports = routes;
