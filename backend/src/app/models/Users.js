b978ce4cc133b6aa126be55f3c490ada7c7f5164
guimkogus <guilherme.mkogus@gmail.com>
2020-02-20 15:40:26 -0300

    encrypting password using BCrypt

--- backend/src/app/models/Users.js
+++ backend/src/app/models/Users.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -6,6 +7,8 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        // Virtual é o tipo de dado que nunca vai existir dentro do Banco de dados, apenas no nosso código
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
@@ -13,7 +16,15 @@ class User extends Model {
         sequelize,
       }
     );
+    // Hooks do Sequelize
+    // Before Save = chamará uma função antes do user ser salvo no banco de dados
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
   }
 }
-
 export default User;
