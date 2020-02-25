cdc5a2626273e9a3fbb764cabfda0acc73b98c00
guimkogus <guilherme.mkogus@gmail.com>
2020-02-25 17:55:50 -0300

    authorization middleware created

---
+++ backend/src/app/middlewares/auth.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  // desta forma desestruturaremos o Header pegando APENAS O token, excluindo o "bearer"
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
+};
