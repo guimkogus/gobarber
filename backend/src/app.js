c806239a22b2f4234e7328caa431d301763c8cf8
guimkogus <guilherme.mkogus@gmail.com>
2020-03-02 14:02:26 -0300

    route to list Service Providers only

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 // Para criarmos nosso canal de comunicação etc
 import express from 'express';
+// Path do NodeJS para indicarmos o caminho do arquivo
+import path from 'path';
 // Onde estará programado todas as nossas rotas de comunicação
 import routes from './routes';
 // Onde será feita a nossa conexão com o banco de dados
@@ -16,6 +18,10 @@ class App {
   middlewares() {
     // Para que a API seja capaz de RECEBER requisições no formato JSON
     this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
