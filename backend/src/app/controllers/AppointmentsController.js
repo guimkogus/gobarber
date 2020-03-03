613fa758c4298f733888d45ca815710203a9bed7
guimkogus <guilherme.mkogus@gmail.com>
2020-03-03 14:24:38 -0300

    appointments date validation

--- backend/src/app/controllers/AppointmentsController.js
+++ backend/src/app/controllers/AppointmentsController.js
@@ -1,5 +1,6 @@
 /* eslint-disable class-methods-use-this */
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointments from '../models/Appointments';
 import User from '../models/User';
 
@@ -25,6 +26,31 @@ class AppointmentsController {
       return res
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
+    }
+
+    // O parseISO transformará uma String de Data para um Objeto de data que o JS reconhece
+    // O startOfHour transformará essa data para o começo de uma hora, ex: 19h30 vira 19h00
+    const hourStart = startOfHour(parseISO(date));
+
+    // Verifica se a hora do agendamento está anterior à data atual
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permited' });
+    }
+
+    // Verifica o prestador possui o horário disponível
+    const checkAvailability = await Appointments.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    // Se encontrar o checkAvailability (for true) significa que o horário não está disponível
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
     }
 
     const appointment = await Appointments.create({
