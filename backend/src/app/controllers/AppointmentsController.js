6e306ae2a4f46e33f145b50bb86f93748973fe4e
guimkogus <guilherme.mkogus@gmail.com>
2020-03-06 16:11:06 -0300

    appointment delete

--- backend/src/app/controllers/AppointmentsController.js
+++ backend/src/app/controllers/AppointmentsController.js
@@ -1,6 +1,6 @@
 /* eslint-disable class-methods-use-this */
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import Appointments from '../models/Appointments';
 import User from '../models/User';
@@ -108,6 +108,32 @@ class AppointmentsController {
       content: `Novo agendamento de ${user.name} para ${formattedDate}`,
       user: provider_id,
     });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointments.findByPk(req.params.id);
+
+    // Check if logged-in user owns this appointment
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment. ",
+      });
+    }
+
+    // Check user are at least 2 hours away from appointment date
+    const atLeast2Hours = subHours(appointment.date, 2);
+
+    if (isBefore(atLeast2Hours, new Date())) {
+      return res
+        .ststus(401)
+        .json({ error: 'You can only cancel appointments 2 hours in advance' });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
 
     return res.json(appointment);
   }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -19,11 +19,17 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
 routes.post('/files', upload.single('file'), FileController.store);
+
 routes.get('/providers', ProviderController.index);
+
 routes.post('/appointments', AppointmentsController.store);
 routes.get('/appointments', AppointmentsController.index);
+routes.delete('/appointments/:id', AppointmentsController.delete);
+
 routes.get('/schedule', ScheduleController.index);
+
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
 
