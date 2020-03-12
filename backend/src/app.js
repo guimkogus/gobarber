3f4471e60fb92e35fda6b6c0134caa4fc8dcce39
guimkogus <guilherme.mkogus@gmail.com>
2020-03-12 16:10:06 -0300

    Sentry exception treatment

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,12 @@
 // Para criarmos nosso canal de comunicação etc
 import express from 'express';
+import 'express-async-errors';
 // Path do NodeJS para indicarmos o caminho do arquivo
 import path from 'path';
+import Youch from 'youch';
+// Sentry para tratamento de exceções
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
 // Onde estará programado todas as nossas rotas de comunicação
 import routes from './routes';
 // Onde será feita a nossa conexão com o banco de dados
@@ -11,11 +16,16 @@ class App {
   constructor() {
     // Agora nossa classe App possui um canal de comunicação. Ex.: this.server.get('/')
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     // Para que a API seja capaz de RECEBER requisições no formato JSON
     this.server.use(express.json());
     this.server.use(
@@ -27,6 +37,15 @@ class App {
   routes() {
     // Importando nossas rotas do arquivo routes.js
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 // Exportando diretamente um novo objeto da classe App e disponibilizando somente atributo "server"
---
+++ backend/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://70642c5757184cb5bb8c189c8bf216c8@sentry.io/4502379',
+};
